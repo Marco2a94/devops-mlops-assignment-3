32efd274fce6d628c871e5fe73a0462f6bba1f05
Marco2a94 <marco.arrighi@efrei.net>
2025-06-11 13:39:17 +0200

    Implement Calculator class with basic arithmetic operations

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,18 @@
 class Calculator:
-    pass
+    def __init__(self):
+        print("Calculator object successfuly initialized")
+    
+    def add(a, b):
+        return a + b
+
+    def subtract(a, b):
+        return a - b
+
+    def multiply(a, b):
+        return a * b
+
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
     
